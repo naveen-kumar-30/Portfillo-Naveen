4dd715d3d046faaf3f08c3d0830bf4e1d3ab8e8f
Naveen Kumar P <naveenkumarp3052004@gmail.com>
2025-05-23 22:39:38 +0530

    Update achievementData.js

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -7,7 +7,7 @@ export const achievementData = {
             details : "Achieved Top 25 among 111 teams at PSG Institute of Technology.",
             date : '28/02/2025',
             field : 'Hackathon',
-            image : 'https://images.unsplash.com/photo-1551836022-d5d88e9218df?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            image : ''
         },
         {
             id : 2,
@@ -15,7 +15,7 @@ export const achievementData = {
             details : 'Completed Core Java Programming Course from LiveWire.',
             date : '30/07/2022',
             field : 'Programming',
-            image : 'https://images.unsplash.com/photo-1581093458790-3c6b2a9c2b0e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            image : ''
         },
         {
             id : 3,
@@ -23,7 +23,7 @@ export const achievementData = {
             details : 'Received Communication Skills Certification from TCS iON.',
             date : '01/02/2024',
             field : 'Communication',
-            image : 'https://images.unsplash.com/photo-1562071707-724f4a91f1c2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+            image : ''
         }
     ]
 }
